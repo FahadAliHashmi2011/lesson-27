26166fcc7922412d780d2f0679caf3466b278aa8
FahadAliHashmi2011 <faiezali2006@gmail>
2025-12-06 15:34:14 +0000

    sc

--- index.py
+++ index.py
@@ -12,3 +12,31 @@ class test_class(Absclass):
 test_obj=test_class()
 test_obj.task ()
 test_obj.print(100)
+##############################################################################################
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+
+class Human(Animal):
+    def move(self):
+        print("I can walk and run")
+class Snake(Animal):
+     def move(self):
+          print("i can crawl")
+class dog(Animal):
+     def move(self):
+          print("i can bark")
+class lion(Animal):
+     def move(self):
+          print("i can roar")
+r = Human()
+r.move()
+k=Snake()
+k.move()
+r=dog()
+r.move()
+k=lion()
+k.move()
